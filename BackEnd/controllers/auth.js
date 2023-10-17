9f3d9f03be2c6d0cd425bd22f3b863d9eeaba399
Abderrazak <abderrazakbouasker@gmail.com>
2023-10-17 22:46:50 +0100

    user login

--- BackEnd/controllers/auth.js
+++ BackEnd/controllers/auth.js
@@ -38,3 +38,23 @@ export const register = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+//LOG IN USER
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user.id }, process.env.jwtSecret);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
